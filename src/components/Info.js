a55a69e7019e54a1566ef79794ca66ee9a33993b
Rahul <rahulramakris@gmail.com>
2022-04-29 12:52:37 -0400

    Added email, LinkedIn icons to btn

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 
 export default function Info() {
     return(
@@ -9,8 +12,8 @@ export default function Info() {
                 <p className="title">JL Founder</p>
                 <p className="email">batman@batmail.com</p>
                 <div className="buttons">
-                    <button className="email--button"><span className="email--icon`"/>Email</button>
-                    <button className="linkedin--button">LinkedIn</button>
+                    <button className="email--button"><EmailIcon fontSize="small" align />Email</button>
+                    <button className="linkedin--button"><LinkedInIcon fontSize="small"/>LinkedIn</button>
                 </div>
             </div>
         </div>
