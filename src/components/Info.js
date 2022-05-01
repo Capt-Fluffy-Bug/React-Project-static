2f561345c46c3f823e2c5a166b2c7f4aa6e6f52c
Rahul <rahulramakris@gmail.com>
2022-04-30 23:24:57 -0400

    centered card using flex

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,7 @@
 import React from "react";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
+import {MdEmail} from "react-icons/md"
 
 export default function Info() {
     return(
@@ -12,7 +12,8 @@ export default function Info() {
                 <p className="title">JL Founder</p>
                 <p className="email">batman@batmail.com</p>
                 <div className="buttons">
-                    <button className="email--button"><EmailIcon fontSize="small" align />Email</button>
+                    {/* <button className="email--button"><span className="email--icon"><MdEmail/></span>Email</button> */}
+                    <button className="email--button"><span className="email--icon"><MdEmail /></span>Email</button>
                     <button className="linkedin--button"><LinkedInIcon fontSize="small"/>LinkedIn</button>
                 </div>
             </div>
